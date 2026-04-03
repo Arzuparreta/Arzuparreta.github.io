03ddd0d0823dff6105a793d712be2396df929445
Arzuparreta <rubenpenarubio81@gmail.com>
2026-04-03 14:58:31 +0200

    update cv source link

--- src/data/site.ts
+++ src/data/site.ts
@@ -4,7 +4,7 @@ export const site = {
 	handle: 'Arzuparreta',
 	url: 'https://arzuparreta.github.io',
 	github: 'https://github.com/Arzuparreta',
-	cvRepo: 'https://github.com/Arzuparreta/CV',
+	cvRepo: 'https://github.com/Arzuparreta/CV/blob/main/CV.md',
 	linkedin: 'https://www.linkedin.com/in/rub%C3%A9n-pe%C3%B1a-432953378/',
 	email: 'rubenpenarubio02@gmail.com',
 	openToRelocation: true,
